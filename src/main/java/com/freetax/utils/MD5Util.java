6c41312ca2a7574538fb55c11bcc2b4205f87a1d
shuxf <shuxianfon1987@126.com>
2017-11-17 10:38:43 +0800

    删除无用方法

--- src/main/java/com/freetax/utils/MD5Util.java
+++ src/main/java/com/freetax/utils/MD5Util.java
@@ -77,41 +77,6 @@ public class MD5Util {
         return resultString;
     }
 
-    /**
-     * 将MD5加密的字符串拼装成jsonString类型：["e","1","0","a","d","c","3","9","4","9","b","a","5","9","a","b","b","e","5","6","e","0","5","7","f","2","0","f","8","8","3","e"]
-     * @param chstr
-     */
-    public static String toJsonString(char[] chstr){
-        StringBuffer strb = new StringBuffer();
-        strb.append("[");
-        for (int i = 0; i < chstr.length; i++){
-            strb.append("\"");
-            strb.append(chstr[i]);
-            strb.append("\"");
-            strb.append(",");
-        }
-        String str = strb.toString().substring(0, strb.toString().length()-1) + "]";
-        return str;
-    }
-
-    /**
-     * ArrayList转化为String
-     * @param lsit
-     */
-    public static String listToString(ArrayList lsit){
-        StringBuffer strb = new StringBuffer();
-        strb.append("[");
-        for (int i=0; i<lsit.size(); i++){
-            strb.append("\"");
-            strb.append(lsit.get(i).toString());
-            strb.append("\"");
-            strb.append(",");
-        }
-        String str = strb.toString().substring(0, strb.toString().length()-1) + "]";
-        return str;
-    }
-
-
     public static void main(String[] args) {
         System.out.println(MD5EncodeByUTF8("123"));
     }
